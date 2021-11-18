3a802a0bc29089fc2b691f85516f08b81ca6cb36
Mikel Bober-Irizar <mikel@mxbi.net>
2021-11-18 22:53:29 +0000

    Improve assignment implementation

--- assignment.py
+++ assignment.py
@@ -33,17 +33,21 @@ class Table:
     def __repr__(self):
         return f'T[{self.n}/{self.size} ({len(self.groups)})]'
 
+    def get_users(self):
+        return [user for g in self.groups for user in g.users]
+
 class NotEnoughTables(Exception):
     pass
 
 class Assignment:
-    def __init__(self, table_size, num_tables, num_nights=2):
+    def __init__(self, table_size, num_tables, num_nights=2, verbose=True):
         # TODO: Support more than two nights
         # This would require a change to the fair splitting algorithm
         assert num_nights == 2
         self.table_size = table_size
         self.num_tables = num_tables
         self.num_nights = num_nights
+        self.verbose = verbose
 
     def fit_group_into_tables(self, group, tables):
         """
@@ -72,7 +76,9 @@ class Assignment:
         """
         total_people = sum([g.n for g in groups])
         num_tables = ceil(total_people / self.table_size)
-        print(f'{total_people} people, making {num_tables}+{extra_tables} tables')
+
+        if self.verbose:
+            print(f'{total_people} people, making {num_tables}+{extra_tables} tables')
 
         tables = [Table(self.table_size) for _ in range(num_tables + extra_tables)]
         
@@ -94,27 +100,39 @@ class Assignment:
         for night, subgroups in groupby(groups_sorted_by_night, key=lambda x: x.dateprefs):
             subgroups = list(subgroups)
 
-            try:
-                tables = self.sort_into_tables(subgroups)
-            except NotEnoughTables:
-                # We may not be able to fit into ceil() tables, so we'll try again with one more table
-                print(f'[WARN] Not enough tables for night `{night}`, increasing by one...')
-                tables = self.sort_into_tables(subgroups, extra_tables=1)
+            extra_tables = 0
+            while True:
+                try:
+                    tables = self.sort_into_tables(subgroups, extra_tables)
+                    break
+                except NotEnoughTables:
+                    # We may not be able to fit into ceil() tables, so we'll try again with one more table
+                    # print(f'[WARN] Not enough tables for night `{night}`, increasing by one...')
+                    extra_tables += 1
+                    print(f'[WARN] Not enough tables for night `{night}`, increasing by one...')
 
             for t in tables:
                 t.night = night
 
             all_tables.extend(tables)
 
-        print(f'{len(all_tables)} total tables, {sum([t.n for t in all_tables])/len(all_tables)} per table')
+        if self.verbose:
+            print(f'{len(all_tables)} total tables, {sum([t.n for t in all_tables])/len(all_tables)} per table')
 
         # We have a list of tables, arranged like this
         # [NIGHT A] [BOTH NIGHTS] [NIGHT B]
         #                ^
         # We split down the middle, and this is how we allocate either to night A or B.
-        n_tables_per_formal = len(all_tables) // self.num_nights
-        night_a_tables = all_tables[:n_tables_per_formal]
-        night_b_tables = all_tables[n_tables_per_formal:]
+        n_tables_per_formal = len(all_tables) / self.num_nights
+        # If we have an uneven split, we flip a coin
+        if n_tables_per_formal % 1 != 0:
+            if np.random.random() < 0.5:
+                n_tables_per_formal += 0.5
+            else:
+                n_tables_per_formal -= 0.5
+
+        night_a_tables = all_tables[:int(n_tables_per_formal)]
+        night_b_tables = all_tables[int(n_tables_per_formal):]
 
         # Make sure that not more than 50% of groups want to go to ONLY A or B
         # If this is the case, we can't allocate using this approach
@@ -132,9 +150,10 @@ class Assignment:
         # Randomly select tables for each night
         night_a_selection = np.random.choice(night_a_tables, self.num_tables, replace=False)
         night_b_selection = np.random.choice(night_b_tables, self.num_tables, replace=False)
-            
-        print(f"Chose {sum([t.n for t in night_a_selection])} for night a:", " ".join(map(repr, night_a_selection)))
-        print(f"Chose {sum([t.n for t in night_b_selection])} for night b:", " ".join(map(repr, night_b_selection)))
+        
+        if self.verbose:
+            print(f"Chose {sum([t.n for t in night_a_selection])} for night a:", " ".join(map(repr, night_a_selection)))
+            print(f"Chose {sum([t.n for t in night_b_selection])} for night b:", " ".join(map(repr, night_b_selection)))
 
         # This assignment might not be full (some tables might still have gap). We need to fill in the gaps
         # This is done by randomly sorting the unsuccessful groups, and then greedily selecting them 
@@ -144,7 +163,8 @@ class Assignment:
             
             num_spaces = sum([t.spaces for t in night_selection])
 
-            print(f"{len(rejected_tables)} tables were rejected of {sum([t.n for t in rejected_tables])} people, with {num_spaces} spaces, filling in")
+            if self.verbose:
+                print(f"{len(rejected_tables)} tables were rejected of {sum([t.n for t in rejected_tables])} people, with {num_spaces} spaces, filling in")
             rejected_groups = [g for t in rejected_tables for g in t.groups]
 
             # Attempt to fit groups in a random order
@@ -157,11 +177,12 @@ class Assignment:
                     pass
 
             num_spaces = sum([t.spaces for t in night_selection])
-            print(f"After filling, we have {num_spaces} spaces left")
-            print(" ".join(map(repr, night_selection)))
+            if self.verbose:
+                print(f"After filling, we have {num_spaces} spaces left")
+                print(" ".join(map(repr, night_selection)))
 
         # Final assignment :)
-        return [night_a_selection, night_b_selection]
+        return [night_a_selection.tolist(), night_b_selection.tolist()]
             
 
 if __name__ == '__main__':
